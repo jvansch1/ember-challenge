af1290730e6b65441bfcfb0ea554c0003d5f4894
Scott Harris <scottharris86@me.com>
2017-05-30 19:16:27 -0400

    base ember application with local storage adapter installed and set up

---
+++ app/adapters/application.js
@@ -0,0 +1,6 @@
+import DS from 'ember-data';
+import LSAdapter from 'ember-localstorage-adapter';
+
+export default LSAdapter.extend({
+    namespace: 'ember-code-challenge'
+});
---
+++ app/serializers/application.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+import { LSSerializer } from 'ember-localstorage-adapter';
+
+export default LSSerializer.extend({
+    
+});
+
