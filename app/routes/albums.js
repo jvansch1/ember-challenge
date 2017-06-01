62b54113b576b78c857ee15b40fb36653b3279dc
John Van Schultz <jvansch1@binghamton.edu>
2017-06-01 13:47:59 -0400

    Add SASS to project

--- app/routes/albums.js
+++ app/routes/albums.js
@@ -4,7 +4,6 @@ export default Ember.Route.extend({
   model: function() {
     let object = JSON.parse(localStorage['ember-code-challenge'])
     let array = [];
-    console.log(object)
     Object.keys(object.album.records).forEach(key => {
       array.push(object.album.records[key])
     })
